3574f04cdd06b531f605cc2879f8e584773750f6
Hari Raghav <123960543+uusername001@users.noreply.github.com>
2024-11-18 20:52:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model= genai.GenerativeModel("gemini-pro")
+def get_gemini_response(question):
+    response=model.generate_content(question)
+    return response.text
+st.set_page_config(page_title="Q&A Demo")
+st.header("Insurance Chatbot")
+input= st.text_input("Input:", key= "Input")
+submit= st.button("Ask the question")
+
+if submit:
+    response = get_gemini_response(input)
+    st.subheader("The Response is")
+    st.write(response)
+
